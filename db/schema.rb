82990bddbd8f9c696e36bf6c9c3da55ae8d6fa54
breizzh <remisamsoninvestisseur@gmail.com>
2018-08-01 16:16:48 +0200

    view terminated

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_122737) do
+ActiveRecord::Schema.define(version: 2018_08_01_135907) do
+
+  create_table "directors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_year"
-    t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "director_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,11 @@
 require 'faker'
 Movie.delete_all
+Director.delete_all
 
+10.times do
+	director = Director.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
 
 50.times do
-	movie = Movie.create(title: Faker::Hipster.word, release_year: Faker::Number.number(4), director: Faker::Name.name)
-end 
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+	movie = Movie.create(title: Faker::Hipster.word, release_year: Faker::Number.number(4), director_id: rand((Director.first.id)..(Director.last.id)))
+end
