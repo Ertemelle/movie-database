abd44bc9f020fbdc5f8e4a65f9b04bd9d53b998e
Ertemelle <phenixime@hotmail.fr>
2018-08-01 15:19:45 +0200

    ajouts de films avec faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+require 'faker'
+Movie.delete_all
+
+
+50.times do
+	movie = Movie.create(title: Faker::Hipster.word, release_year: Faker::Number.number(4), director: Faker::Name.name)
+end 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
